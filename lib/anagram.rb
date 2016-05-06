c4eaa672bcf588bccb3bbe74a5bffabf0da16d81
tmharkins <tmharkins@gmail.com>
2016-05-06 13:18:41 +0000

    nitrous-backup at Fri May  6 13:18:41 UTC 2016

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+	
+  attr_accessor :anagram
+	
+  def initialize(anagram)
+		@anagram = anagram
+	end
+
+	def match(list)
+		matches = []
+		list.each do |word|
+			list_sort = word.chars.sort { |a, b| a.casecmp(b) } .join
+			anagram_sort = anagram.chars.sort { |a, b| a.casecmp(b) } .join
+			matches << word if list_sort == anagram_sort
+		end
+		matches
+	end
+end
